f4a4eb54305758a4381e99f593a1b86f288ff8fa
ganeshwatte123 <watteganesh41@gmail.com>
2025-06-10 20:47:42 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "three" {
+  ami           = "ami-0dc3a08bd93f84a35"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "abc-server"
+  }
+ }
