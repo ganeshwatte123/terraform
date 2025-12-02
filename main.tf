5d8a68b25d797fb175069e74ac9bfe259b6610b3
ganeshwatte123 <watteganesh41@gmail.com>
2025-12-02 13:10:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
-
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
-
 resource "aws_instance" "one" {
-  ami           = "ami-0ace6d6fe60486ca2c"
+  ami           = "ami-0cae6d6fe6048ca2c"
   instance_type = "t2.micro"
-
   tags = {
-    Name = "hcp-server"
+    Name = "module-server"
   }
 }
