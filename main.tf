9527f4feb5879fd8dcd54d45288c314ad8228c58
ganeshwatte123 <watteganesh41@gmail.com>
2025-12-02 13:24:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  cloud {
-    organization = "swiggydemoinfrakush"    # तुमचा organization name इथे
-    workspaces {
-      name = "terraform"                     # तुमचा workspace name
-    }
-  }
-}
-
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
+
 
 resource "aws_instance" "one" {
   ami           = "ami-0cae6d6fe6048ca2c"
   instance_type = "t2.micro"
-
   tags = {
     Name = "hcp-server"
   }
